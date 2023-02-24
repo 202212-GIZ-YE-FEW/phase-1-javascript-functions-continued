4374471e1f41b770150db657062c4ef8fc6a256b
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-02-24 07:42:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activety){
+    return `This Saturday, I want to ${activety}!`
+}
+
+function mondayWork(activety){
+     return `This Saturday, I want to ${activety}!`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
